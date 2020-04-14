9db87fc1fb4b47a2e302db90c21882f9e3bcd77a
ros <rk65536@protonmail.com>
2020-04-14 11:20:47 +0100

    add spring boot template

---
+++ src/main/java/pt/inesctec/adcauthmiddleware/AdcAuthMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package pt.inesctec.adcauthmiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdcAuthMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdcAuthMiddlewareApplication.class, args);
+	}
+
+}
