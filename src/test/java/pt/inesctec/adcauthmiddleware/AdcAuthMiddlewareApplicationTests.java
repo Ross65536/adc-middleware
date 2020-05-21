af7a501f3c8c782bf6da0f7be3b5e4302675cd13
ros <rk65536@protonmail.com>
2020-05-21 16:00:06 +0100

    add info endpoint test

--- src/test/java/pt/inesctec/adcauthmiddleware/AdcAuthMiddlewareApplicationTests.java
+++ src/test/java/pt/inesctec/adcauthmiddleware/AdcAuthMiddlewareApplicationTests.java
@@ -1,8 +1,11 @@
 package pt.inesctec.adcauthmiddleware;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import java.util.Map;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
@@ -10,11 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AdcAuthMiddlewareApplicationTests {
+  private static ObjectMapper JsonObjectMapper = new ObjectMapper();
 
   @LocalServerPort
   private int port;
@@ -28,23 +33,39 @@ class AdcAuthMiddlewareApplicationTests {
   public void init() {
   }
 
-  @Test
-  void publicInfo() {
-    int status = 200;
+  private static String toJson(Object obj) throws JsonProcessingException {
+    return JsonObjectMapper.writeValueAsString(obj);
+  }
 
-    backend.stubFor(WireMock.get("/airr/v1/info")
+  private static String JSON_MIME = "application/json";
+
+  public static void setupGetJsonMock(WireMockServer mock, String url, int status, String json) {
+    mock.stubFor(WireMock.get(url)
+        .withHeader("Accept",  containing(JSON_MIME))
         .willReturn(WireMock.aResponse()
-          .withStatus(status)
-          .withBody("{}")
-            .withHeader("Content-Type", "application/json")
+            .withStatus(status)
+            .withBody(json)
+            .withHeader("Content-Type", JSON_MIME)
         )
     );
+  }
+
+  @Test
+  void publicInfo() throws JsonProcessingException {
+    int status = 200;
+    String path = "/airr/v1/info";
+    var info = Map.of(
+        "name", "airr",
+        "last_update", 123
+    );
+    var respJson = toJson(info);
+
+    setupGetJsonMock(backend, path, status, respJson);
     backend.start();
 
-
-    var entity = this.restTemplate.getForEntity("http://localhost:" + port + "/airr/v1/info", String.class);
+    var entity = this.restTemplate.getForEntity("http://localhost:" + port + path, String.class);
     assertThat(entity.getStatusCodeValue()).isEqualTo(status);
-
+    assertThat(entity.getBody()).isEqualTo(respJson);
 
   }
 }
