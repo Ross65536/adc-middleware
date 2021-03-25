f714adbe3365072354015e1c6adef7b3e5c8aab3
Edgar Silva <23741809+edgar-simao@users.noreply.github.com>
2021-03-25 17:08:05 +0000

    minor changes

--- src/main/java/pt/inesctec/adcauthmiddleware/controllers/ResourceController.java
+++ src/main/java/pt/inesctec/adcauthmiddleware/controllers/ResourceController.java
@@ -1,22 +1,9 @@
 package pt.inesctec.adcauthmiddleware.controllers;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import pt.inesctec.adcauthmiddleware.config.AppConfig;
-import pt.inesctec.adcauthmiddleware.db.dto.TemplateDTO;
-import pt.inesctec.adcauthmiddleware.db.dto.TemplatesListDTO;
-import pt.inesctec.adcauthmiddleware.db.models.Templates;
-import pt.inesctec.adcauthmiddleware.db.repository.TemplatesRepository;
 
 @RequestMapping("/resource")
 public abstract class ResourceController {
