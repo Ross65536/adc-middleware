c62fe4ccf318529a5e72d5b964393b3592351d3d
Edgar Silva <23741809+edgar-simao@users.noreply.github.com>
2021-09-29 19:10:23 +0100

    changing ownership authz

--- src/main/java/pt/inesctec/adcauthmiddleware/controllers/authz/OwnResourcesController.java
+++ src/main/java/pt/inesctec/adcauthmiddleware/controllers/authz/OwnResourcesController.java
@@ -1,20 +1,24 @@
 package pt.inesctec.adcauthmiddleware.controllers.authz;
 
+import com.google.common.collect.ImmutableMap;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pt.inesctec.adcauthmiddleware.controllers.AuthzController;
 import pt.inesctec.adcauthmiddleware.http.HttpFacade;
 import pt.inesctec.adcauthmiddleware.http.HttpRequestBuilderFacade;
+import pt.inesctec.adcauthmiddleware.http.SyncException;
 import pt.inesctec.adcauthmiddleware.utils.SpringUtils;
 import pt.inesctec.adcauthmiddleware.utils.Utils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.Map;
 
 @RestController
 public class OwnResourcesController extends AuthzController {
@@ -52,5 +56,36 @@ public class OwnResourcesController extends AuthzController {
             throw e;
         }
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/change_owner/{resource_id}/{new_owner}", method = RequestMethod.POST)
+    public ResponseEntity<String> changeOwner(
+            @PathVariable String resource_id,
+            @PathVariable String new_owner,
+            HttpServletRequest request
+    ) throws Exception {
+        String bearer = SpringUtils.getBearer(request);
+        checkRequestValidity(bearer);
+
+        String stringUri = umaClient.getKeycloakExtensionApiUri()
+                + "/change_owner/"
+                + resource_id + "/"
+                + new_owner;
+        var uri = Utils.buildUrl(stringUri);
+
+        var toRequest = new HttpRequestBuilderFacade()
+                .postForm(uri, ImmutableMap.of())
+                .withBearer(bearer)
+                .expectJson()
+                .build();
+
+        try {
+            HttpFacade.makeRequest(toRequest);
+        } catch (Exception e) {
+            Logger.error("Failed to change owner beacuse: " + e.getMessage());
+            throw e;
+        }
+
+        return new ResponseEntity<>("", HttpStatus.OK);
     }
 }
